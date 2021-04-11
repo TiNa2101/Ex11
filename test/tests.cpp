684833c69db031338db720c6e0c344fcefb53870
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-04-11 22:46:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,18 +5,18 @@
 
 TEST(TrainTest, test1) {
   Train train1;
-  train1->createCages(20);
+  train1.createCages(20);
   EXPECT_EQ(20, train1.findLength());
 }
 
 TEST(TrainTest, test2) {
   Train train2;
-  train2->createCages(15);
+  train2.createCages(15);
   EXPECT_EQ(15, train2.findLength());
 }
 
 TEST(TrainTest, test3) {
   Train train3;
-  train3->createCages(33);
+  train3.createCages(33);
   EXPECT_EQ(33, train3.findLength());
 }
